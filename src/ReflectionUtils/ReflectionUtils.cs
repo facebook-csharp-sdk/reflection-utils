ad5b68e5f6ac56f82085a2e7fda3cf6ea2edf158
prabirshrestha <mail@prabir.me>
2012-03-04 19:42:55 -0500

    updated for Win8 CTP and VS11 beta use NETFX_CORE symbole

--- src/ReflectionUtils/ReflectionUtils.cs
+++ src/ReflectionUtils/ReflectionUtils.cs
@@ -36,7 +36,7 @@ namespace ReflectionUtils
     {
         public static Attribute GetAttribute(MemberInfo info, Type type)
         {
-#if REFLECTION_UTILS_WINRT
+#if NETFX_CORE
             if (info == null || type == null || !info.IsDefined(type))
                 return null;
             return info.GetCustomAttribute(type);
@@ -50,7 +50,7 @@ namespace ReflectionUtils
         public static Attribute GetAttribute(Type objectType, Type attributeType)
         {
 
-#if REFLECTION_UTILS_WINRT
+#if NETFX_CORE
             if (objectType == null || attributeType == null || !objectType.GetTypeInfo().IsDefined(attributeType))
                 return null;
             return objectType.GetTypeInfo().GetCustomAttribute(attributeType);
@@ -63,7 +63,11 @@ namespace ReflectionUtils
 
         public static bool IsTypeGenericeCollectionInterface(Type type)
         {
+#if NETFX_CORE
+            if (!type.GetTypeInfo().IsGenericType)
+#else
             if (!type.IsGenericType)
+#endif
                 return false;
 
             Type genericDefinition = type.GetGenericTypeDefinition();
@@ -73,66 +77,69 @@ namespace ReflectionUtils
 
         public static bool IsTypeDictionary(Type type)
         {
-#if REFLECTION_UTILS_WINRT
+#if NETFX_CORE
             if (typeof(IDictionary<,>).GetTypeInfo().IsAssignableFrom(type.GetTypeInfo()))
                 return true;
-#else
-            if (typeof(IDictionary).IsAssignableFrom(type))
-                return true;
-#endif
 
-            if (!type.IsGenericType)
+            if (!type.GetTypeInfo().IsGenericType)
                 return false;
+#else
+                if (typeof(IDictionary).IsAssignableFrom(type))
+                    return true;
 
+                if (!type.IsGenericType)
+                    return false;
+#endif
             Type genericDefinition = type.GetGenericTypeDefinition();
             return genericDefinition == typeof(IDictionary<,>);
         }
     }
+}
 
 #if REFLECTION_UTILS_INTERNAL
     internal
 #else
-    public
+public
 #endif
  delegate object GetHandler(object source);
 
 #if REFLECTION_UTILS_INTERNAL
     internal
 #else
-    public
+public
 #endif
  delegate void SetHandler(object source, object value);
 
 #if REFLECTION_UTILS_INTERNAL
     internal
 #else
-    public
+public
 #endif
  delegate void MemberMapLoader(Type type, SafeDictionary<string, CacheResolver.MemberMap> memberMaps);
 
 #if REFLECTION_UTILS_INTERNAL
     internal
 #else
-    public
+public
 #endif
  class CacheResolver
+{
+    private readonly MemberMapLoader _memberMapLoader;
+    private readonly SafeDictionary<Type, SafeDictionary<string, MemberMap>> _memberMapsCache = new SafeDictionary<Type, SafeDictionary<string, MemberMap>>();
+
+    delegate object CtorDelegate();
+    readonly static SafeDictionary<Type, CtorDelegate> ConstructorCache = new SafeDictionary<Type, CtorDelegate>();
+
+    public CacheResolver(MemberMapLoader memberMapLoader)
     {
-        private readonly MemberMapLoader _memberMapLoader;
-        private readonly SafeDictionary<Type, SafeDictionary<string, MemberMap>> _memberMapsCache = new SafeDictionary<Type, SafeDictionary<string, MemberMap>>();
+        _memberMapLoader = memberMapLoader;
+    }
 
-        delegate object CtorDelegate();
-        readonly static SafeDictionary<Type, CtorDelegate> ConstructorCache = new SafeDictionary<Type, CtorDelegate>();
-
-        public CacheResolver(MemberMapLoader memberMapLoader)
-        {
-            _memberMapLoader = memberMapLoader;
-        }
-
-        public static object GetNewInstance(Type type)
-        {
-            CtorDelegate c;
-            if (ConstructorCache.TryGetValue(type, out c))
-                return c();
+    public static object GetNewInstance(Type type)
+    {
+        CtorDelegate c;
+        if (ConstructorCache.TryGetValue(type, out c))
+            return c();
 #if REFLECTION_UTILS_REFLECTIONEMIT
                 DynamicMethod dynamicMethod = new DynamicMethod("Create" + type.FullName, typeof(object), Type.EmptyTypes, type, true);
                 dynamicMethod.InitLocals = true;
@@ -155,38 +162,38 @@ namespace ReflectionUtils
                 ConstructorCache.Add(type, c);
                 return c();
 #else
-#if REFLECTION_UTILS_WINRT
-            IEnumerable<ConstructorInfo> constructorInfos = type.GetTypeInfo().DeclaredConstructors;
-            ConstructorInfo constructorInfo = null;
-            foreach (ConstructorInfo item in constructorInfos) // FirstOrDefault()
+#if NETFX_CORE
+        IEnumerable<ConstructorInfo> constructorInfos = type.GetTypeInfo().DeclaredConstructors;
+        ConstructorInfo constructorInfo = null;
+        foreach (ConstructorInfo item in constructorInfos) // FirstOrDefault()
+        {
+            if (item.GetParameters().Length == 0) // Default ctor - make sure it doesn't contain any parameters
             {
-                if (item.GetParameters().Length == 0) // Default ctor - make sure it doesn't contain any parameters
-                {
-                    constructorInfo = item;
-                    break;
-                }
+                constructorInfo = item;
+                break;
             }
+        }
 #else
             ConstructorInfo constructorInfo = type.GetConstructor(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, null, Type.EmptyTypes, null);
 #endif
-            c = delegate { return constructorInfo.Invoke(null); };
-            ConstructorCache.Add(type, c);
-            return c();
+        c = delegate { return constructorInfo.Invoke(null); };
+        ConstructorCache.Add(type, c);
+        return c();
 #endif
-        }
+    }
 
-        public SafeDictionary<string, MemberMap> LoadMaps(Type type)
-        {
-            if (type == null || type == typeof(object))
-                return null;
-            SafeDictionary<string, MemberMap> maps;
-            if (_memberMapsCache.TryGetValue(type, out maps))
-                return maps;
-            maps = new SafeDictionary<string, MemberMap>();
-            _memberMapLoader(type, maps);
-            _memberMapsCache.Add(type, maps);
+    public SafeDictionary<string, MemberMap> LoadMaps(Type type)
+    {
+        if (type == null || type == typeof(object))
+            return null;
+        SafeDictionary<string, MemberMap> maps;
+        if (_memberMapsCache.TryGetValue(type, out maps))
             return maps;
-        }
+        maps = new SafeDictionary<string, MemberMap>();
+        _memberMapLoader(type, maps);
+        _memberMapsCache.Add(type, maps);
+        return maps;
+    }
 
 #if REFLECTION_UTILS_REFLECTIONEMIT
             static DynamicMethod CreateDynamicMethod(string name, Type returnType, Type[] parameterTypes, Type owner)
@@ -199,8 +206,8 @@ namespace ReflectionUtils
             }
 #endif
 
-        static GetHandler CreateGetHandler(FieldInfo fieldInfo)
-        {
+    static GetHandler CreateGetHandler(FieldInfo fieldInfo)
+    {
 #if REFLECTION_UTILS_REFLECTIONEMIT
                 Type type = fieldInfo.FieldType;
                 DynamicMethod dynamicGet = CreateDynamicMethod("Get" + fieldInfo.Name, fieldInfo.DeclaringType, new Type[] { typeof(object) }, fieldInfo.DeclaringType);
@@ -214,14 +221,14 @@ namespace ReflectionUtils
 
                 return (GetHandler)dynamicGet.CreateDelegate(typeof(GetHandler));
 #else
-            return delegate(object instance) { return fieldInfo.GetValue(instance); };
+        return delegate(object instance) { return fieldInfo.GetValue(instance); };
 #endif
-        }
+    }
 
-        static SetHandler CreateSetHandler(FieldInfo fieldInfo)
-        {
-            if (fieldInfo.IsInitOnly || fieldInfo.IsLiteral)
-                return null;
+    static SetHandler CreateSetHandler(FieldInfo fieldInfo)
+    {
+        if (fieldInfo.IsInitOnly || fieldInfo.IsLiteral)
+            return null;
 #if REFLECTION_UTILS_REFLECTIONEMIT
                 Type type = fieldInfo.FieldType;
                 DynamicMethod dynamicSet = CreateDynamicMethod("Set" + fieldInfo.Name, null, new Type[] { typeof(object), typeof(object) }, fieldInfo.DeclaringType);
@@ -236,19 +243,19 @@ namespace ReflectionUtils
 
                 return (SetHandler)dynamicSet.CreateDelegate(typeof(SetHandler));
 #else
-            return delegate(object instance, object value) { fieldInfo.SetValue(instance, value); };
+        return delegate(object instance, object value) { fieldInfo.SetValue(instance, value); };
 #endif
-        }
+    }
 
-        static GetHandler CreateGetHandler(PropertyInfo propertyInfo)
-        {
-#if REFLECTION_UTILS_WINRT
-            MethodInfo getMethodInfo = propertyInfo.GetMethod;
+    static GetHandler CreateGetHandler(PropertyInfo propertyInfo)
+    {
+#if NETFX_CORE
+        MethodInfo getMethodInfo = propertyInfo.GetMethod;
 #else
             MethodInfo getMethodInfo = propertyInfo.GetGetMethod(true);
 #endif
-            if (getMethodInfo == null)
-                return null;
+        if (getMethodInfo == null)
+            return null;
 #if REFLECTION_UTILS_REFLECTIONEMIT
                 Type type = propertyInfo.PropertyType;
                 DynamicMethod dynamicGet = CreateDynamicMethod("Get" + propertyInfo.Name, propertyInfo.DeclaringType, new Type[] { typeof(object) }, propertyInfo.DeclaringType);
@@ -262,23 +269,23 @@ namespace ReflectionUtils
 
                 return (GetHandler)dynamicGet.CreateDelegate(typeof(GetHandler));
 #else
-#if REFLECTION_UTILS_WINRT
-            return delegate(object instance) { return getMethodInfo.Invoke(instance, new Type[] { }); };
+#if NETFX_CORE
+        return delegate(object instance) { return getMethodInfo.Invoke(instance, new Type[] { }); };
 #else
             return delegate(object instance) { return getMethodInfo.Invoke(instance, Type.EmptyTypes); };
 #endif
 #endif
-        }
+    }
 
-        static SetHandler CreateSetHandler(PropertyInfo propertyInfo)
-        {
-#if REFLECTION_UTILS_WINRT
-            MethodInfo setMethodInfo = propertyInfo.SetMethod;
+    static SetHandler CreateSetHandler(PropertyInfo propertyInfo)
+    {
+#if NETFX_CORE
+        MethodInfo setMethodInfo = propertyInfo.SetMethod;
 #else
             MethodInfo setMethodInfo = propertyInfo.GetSetMethod(true);
 #endif
-            if (setMethodInfo == null)
-                return null;
+        if (setMethodInfo == null)
+            return null;
 #if REFLECTION_UTILS_REFLECTIONEMIT
                 Type type = propertyInfo.PropertyType;
                 DynamicMethod dynamicSet = CreateDynamicMethod("Set" + propertyInfo.Name, null, new Type[] { typeof(object), typeof(object) }, propertyInfo.DeclaringType);
@@ -292,38 +299,8 @@ namespace ReflectionUtils
                 setGenerator.Emit(OpCodes.Ret);
                 return (SetHandler)dynamicSet.CreateDelegate(typeof(SetHandler));
 #else
-            return delegate(object instance, object value) { setMethodInfo.Invoke(instance, new[] { value }); };
+        return delegate(object instance, object value) { setMethodInfo.Invoke(instance, new[] { value }); };
 #endif
-        }
-
-#if REFLECTION_UTILS_INTERNAL
-    internal
-#else
-        public
-#endif
- sealed class MemberMap
-        {
-            public readonly MemberInfo MemberInfo;
-            public readonly Type Type;
-            public readonly GetHandler Getter;
-            public readonly SetHandler Setter;
-
-            public MemberMap(PropertyInfo propertyInfo)
-            {
-                MemberInfo = propertyInfo;
-                Type = propertyInfo.PropertyType;
-                Getter = CreateGetHandler(propertyInfo);
-                Setter = CreateSetHandler(propertyInfo);
-            }
-
-            public MemberMap(FieldInfo fieldInfo)
-            {
-                MemberInfo = fieldInfo;
-                Type = fieldInfo.FieldType;
-                Getter = CreateGetHandler(fieldInfo);
-                Setter = CreateSetHandler(fieldInfo);
-            }
-        }
     }
 
 #if REFLECTION_UTILS_INTERNAL
@@ -331,33 +308,62 @@ namespace ReflectionUtils
 #else
     public
 #endif
- class SafeDictionary<TKey, TValue>
+ sealed class MemberMap
     {
-        private readonly object _padlock = new object();
-        private readonly Dictionary<TKey, TValue> _dictionary = new Dictionary<TKey, TValue>();
+        public readonly MemberInfo MemberInfo;
+        public readonly Type Type;
+        public readonly GetHandler Getter;
+        public readonly SetHandler Setter;
 
-        public bool TryGetValue(TKey key, out TValue value)
+        public MemberMap(PropertyInfo propertyInfo)
         {
-            return _dictionary.TryGetValue(key, out value);
+            MemberInfo = propertyInfo;
+            Type = propertyInfo.PropertyType;
+            Getter = CreateGetHandler(propertyInfo);
+            Setter = CreateSetHandler(propertyInfo);
         }
 
-        public TValue this[TKey key]
+        public MemberMap(FieldInfo fieldInfo)
         {
-            get { return _dictionary[key]; }
+            MemberInfo = fieldInfo;
+            Type = fieldInfo.FieldType;
+            Getter = CreateGetHandler(fieldInfo);
+            Setter = CreateSetHandler(fieldInfo);
         }
+    }
+}
 
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-        {
-            return ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).GetEnumerator();
-        }
+#if REFLECTION_UTILS_INTERNAL
+    internal
+#else
+public
+#endif
+ class SafeDictionary<TKey, TValue>
+{
+    private readonly object _padlock = new object();
+    private readonly Dictionary<TKey, TValue> _dictionary = new Dictionary<TKey, TValue>();
 
-        public void Add(TKey key, TValue value)
+    public bool TryGetValue(TKey key, out TValue value)
+    {
+        return _dictionary.TryGetValue(key, out value);
+    }
+
+    public TValue this[TKey key]
+    {
+        get { return _dictionary[key]; }
+    }
+
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        return ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).GetEnumerator();
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        lock (_padlock)
         {
-            lock (_padlock)
-            {
-                if (_dictionary.ContainsKey(key) == false)
-                    _dictionary.Add(key, value);
-            }
+            if (_dictionary.ContainsKey(key) == false)
+                _dictionary.Add(key, value);
         }
     }
 }
